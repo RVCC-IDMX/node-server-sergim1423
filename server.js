1464aff5e274bc3813c0dad7ab4b0c1551692732
Sergius Manolov <67722057+sergim1423@users.noreply.github.com>
2022-10-27 00:48:22 -0400

    global properties

--- server.js
+++ server.js
@@ -1,15 +1,2 @@
-/* eslint-disable no-unused-vars */
-const http = require('http');
-
-const HOSTNAME = process.env.HOSTNAME || 'localhost';
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello World');
-});
-
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server is running at http://${HOSTNAME}:${PORT}/`);
-});
+console.log(__filename);
+console.log(__dirname);
