2625a728c393d4e50645c0459609750940eff9e2
Sergius Manolov <67722057+sergim1423@users.noreply.github.com>
2022-10-27 01:41:00 -0400

    (10) basic web server with html files

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World<h1>');
+  fs.readFile('./index.html', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
