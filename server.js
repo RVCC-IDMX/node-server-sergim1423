dd901a25e7981a932ca2de2d5f37560a9c68a6ec
Sergius Manolov <67722057+sergim1423@users.noreply.github.com>
2022-10-27 01:12:45 -0400

    deleting files

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const { rename } = require('fs');
+const { unlink } = require('fs');
 
-rename('hi,txt', 'hello.txt', (err) => {
+unlink('hello.txt', (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log('File renamed!');
+  console.log('File deleted!');
 });
